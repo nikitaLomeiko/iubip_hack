3813735a96c6993ff9894b315899fa3be0ffee37
Nikita <nikitalomieiko@gmail.com>
2025-04-11 19:54:43 +0300

    feat: init & fsd

---
+++ src/app/App.tsx
@@ -0,0 +1,12 @@
+import './styles/index.css'
+
+function App() {
+  return (
+    <div className="text-red-500">
+      тут будет крутой сайт
+      
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      app: "/src/app",
+      pages: "/src/pages",
+      widgets: "/src/widgets",
+      features: "/src/features",
+      entities: "/src/entities",
+      shared: "/src/shared",
+    },
+  },
+});
